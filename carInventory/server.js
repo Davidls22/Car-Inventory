86f7a0b7c9810d98134d8c5271fb5d7bcd7d56e0
Davidls22 <116105660+Davidls22@users.noreply.github.com>
2023-04-13 10:22:58 +0100

    Update server.js

--- carInventory/server.js
+++ carInventory/server.js
@@ -1,60 +1,21 @@
-// Import necessary modules
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
+const { connectToDatabase } = require('./db');
 
-// Create Express app
 const app = express();
-
-// Import routers for handling API requests
-const carsRouter = require('./server/routes/cars');
-
-// Use middleware for handling JSON data in requests
-app.use(express.json());
-
-// Set port for the app to listen on
 const port = process.env.PORT || 8082;
 
-// Connect to MongoDB
-const uri = 'mongodb+srv://davidsugden1:EnwIZby1w5CmO2js@clustercar.ykjjobu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-mongoose.Promise = global.Promise;
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Successfully connected to MongoDB'))
-.catch(err => {
-  console.error('Error connecting to MongoDB', err.message);
-  process.exit(1);
-});
-
-mongoose.connection.on('connected', () => {
-  console.log('Mongoose connected to MongoDB');
-});
-
-mongoose.connection.on('error', (err) => {
-  console.error(`Mongoose connection error: ${err}`);
-});
-
-mongoose.connection.on('disconnected', () => {
-  console.warn('Mongoose disconnected from MongoDB');
-});
-
-process.on('SIGINT', () => {
-  mongoose.connection.close(() => {
-    console.log('Mongoose connection disconnected through app termination');
-    process.exit(0);
-  });
-});
-
-// Set up middleware
 app.use(cors());
 app.use(express.json());
 
-// Set up routes
 app.use('/api/cars', carsRouter);
 
-module.exports = app;
+async function startServer() {
+  await connectToDatabase();
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
+}
 
-// Start the server
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+startServer();
+
