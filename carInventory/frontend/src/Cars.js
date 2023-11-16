01258a166dcd6f7f4e5335862cfa3978c850be13
David Sugden <116105660+Davidls22@users.noreply.github.com>
2023-11-16 21:48:32 +0000

    Update Cars.js

--- carInventory/frontend/src/Cars.js
+++ carInventory/frontend/src/Cars.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8082';
+const API_BASE_URL = 'https://carinventorybackend.onrender.com';
 
 function App() {
   const [cars, setCars] = useState([]);
